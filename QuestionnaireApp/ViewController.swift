c6e4421db30d4619632dfb1c1b56b230fc670783
Adnan Hussain <adnanhussaiin98@gmail.com>
2019-11-19 21:55:29 +0000

    first commit

--- QuestionnaireApp/ViewController.swift
+++ QuestionnaireApp/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    //initialising asnwers variable for data to be passed through
+    var answers: Data?
+    
+    //initalises the variable that stores the answer yes or no as a string
+    var storedAnswers: String!
 
+    //connects to the segue identifier, moves onto next page
+    @IBAction func enterButton(_ sender: Any) {
+        performSegue(withIdentifier: "enter", sender: self)
+    }
+    
+    //connects to the segue identifier, moves onto admin login page
+    @IBAction func adminButton(_ sender: Any) {
+        performSegue(withIdentifier: "viewResults", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
-
